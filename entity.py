53e5caab92cbf237ae34c81fe52af6b488ecb85f
lemDace <dace1717@gmail.com>
2025-09-24 21:49:08 +0000

    created entities for first time

--- entity.py
+++ entity.py
@@ -1,3 +1,27 @@
 import euclid3
 from euclid3 import Vector2
 
+class Player:
+    
+    def __init__(self, name:str,team:str,speed:float, position:Vector2):
+        self.name = name
+        self.team = team
+        self.speed = speed
+        self.position = position
+
+
+
+class Team:
+
+    def __init__(self, name:str):
+        self.name = name
+
+
+
+
+class Ball:
+
+    def __init__(self, position:Vector2):
+        self.position = position        
+
+
