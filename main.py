e03436bec7d7eadcf1e0bffb94f1b8a59a268dae
lemDace <dace1717@gmail.com>
2025-09-25 02:09:33 +0000

    adding some code

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import euclid3
+import entity
+from entity import *
 
+testPlayer = Player("Lee","TeamA",3,(3,3))
+
+teamA: list[Player] = []
+
+teamA.append(testPlayer)
+
+print("Players name is: ", teamA[0].name)
 
