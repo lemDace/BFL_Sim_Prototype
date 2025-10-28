1d56788aa4c1d871659e7a22785b863a8ebe5300
lemDace <dace1717@gmail.com>
2025-10-28 02:41:00 +0000

    added NewPlayer class

--- entity/__init__.py
+++ entity/__init__.py
@@ -1,5 +1,8 @@
+from .entity import Entity, PhysicalEntity
 from .field import Field
 from .ball import Ball
 from .team import Team
 from .player import Player
+from .newPlayer import NewPlayer
+
 #from .match import Match
---
+++ entity/newPlayer.py
@@ -0,0 +1,7 @@
+from entity import PhysicalEntity
+
+#newPlayer class to eventually replace the Player class.
+#this class will inherit from physicalEntity()
+
+class NewPlayer(PhysicalEntity):
+    #new class def
