98de9d0b8bc174637f873c6d37a275b554e704c5
lemDace <dace1717@gmail.com>
2025-11-11 21:04:49 +0000

    running version

--- entity/__init__.py
+++ entity/__init__.py
@@ -3,6 +3,6 @@ from .field import Field
 from .ball import Ball
 from .team import Team
 from .player import Player
-from .newPlayer import NewPlayer
+#from .newPlayer import NewPlayer
 
 #from .match import Match
